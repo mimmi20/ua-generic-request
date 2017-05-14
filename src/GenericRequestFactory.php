78cd56b7e069ea6dd131aa5c1ac0d21597a3af78
Thomas Müller <mimmi20@live.de>
2017-05-14 17:26:39 +0200

    add support for PSR-7 message interface

--- src/GenericRequestFactory.php
+++ src/GenericRequestFactory.php
@@ -11,6 +11,8 @@
 declare(strict_types = 1);
 namespace Wurfl\Request;
 
+use Psr\Http\Message\MessageInterface;
+
 /**
  * Creates a Generic WURFL Request from the raw HTTP Request
  */
@@ -23,8 +25,23 @@ class GenericRequestFactory
      * @param bool  $overrideSideloadedBrowserUa
      *
      * @return \Wurfl\Request\GenericRequest
+     *
+     * @deprecated use {@see createRequestFromArray} instead
      */
     public function createRequest(array $request, $overrideSideloadedBrowserUa = true)
+    {
+        return $this->createRequestFromArray($request, $overrideSideloadedBrowserUa);
+    }
+
+    /**
+     * Creates Generic Request from the given HTTP Request (normally $_SERVER)
+     *
+     * @param array $request                     HTTP Request
+     * @param bool  $overrideSideloadedBrowserUa
+     *
+     * @return \Wurfl\Request\GenericRequest
+     */
+    public function createRequestFromArray(array $request, $overrideSideloadedBrowserUa = true)
     {
         return new GenericRequest($request, $overrideSideloadedBrowserUa);
     }
@@ -35,10 +52,42 @@ class GenericRequestFactory
      * @param string $userAgent
      *
      * @return \Wurfl\Request\GenericRequest
+     *
+     * @deprecated use {@see createRequestFromString} instead
      */
     public function createRequestForUserAgent($userAgent)
     {
+        return $this->createRequestFromString($userAgent);
+    }
+
+    /**
+     * Create a Generic Request from the given $userAgent
+     *
+     * @param string $userAgent
+     *
+     * @return \Wurfl\Request\GenericRequest
+     */
+    public function createRequestFromString($userAgent)
+    {
         return new GenericRequest([Constants::HEADER_HTTP_USERAGENT => $userAgent]);
+    }
+
+    /**
+     * Create a Generic Request from the given $userAgent
+     *
+     * @param \Psr\Http\Message\MessageInterface $message
+     *
+     * @return \Wurfl\Request\GenericRequest
+     */
+    public function createRequestFromPsr7Message(MessageInterface $message)
+    {
+        $headers = [];
+
+        foreach ($message->getHeaders() as $name => $values) {
+            $headers[$name] = implode(', ', $values);
+        }
+
+        return new GenericRequest($headers);
     }
 
     /**
