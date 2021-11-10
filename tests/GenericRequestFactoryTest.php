3bfc4b15dc1f907f715b8867df4eb1003e440bdd
Thomas Müller <mimmi20@live.de>
2021-11-10 17:26:57 +0100

    update pipeline (#263)

--- tests/GenericRequestFactoryTest.php
+++ tests/GenericRequestFactoryTest.php
@@ -12,7 +12,7 @@ declare(strict_types = 1);
 
 namespace UaRequestTest;
 
-use ExceptionalJSON\DecodeErrorException;
+use JsonClass\DecodeErrorException;
 use JsonClass\Json;
 use Laminas\Diactoros\ServerRequestFactory;
 use LogicException;
